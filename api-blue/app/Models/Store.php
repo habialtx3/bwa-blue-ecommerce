b025e05a83721b4fe6b6b61405bdc471f55971f2
MREZA <rezaworku31@gmail.com>
2025-12-28 00:32:07 +0700

    created : store migration and user-store relation

---
+++ api-blue/app/Models/Store.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\UUID;
+
+class Store extends Model
+{
+    //
+    use UUID;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'logo',
+        'about',
+        'phone',
+        'address_id',
+        'city',
+        'address',
+        'postal_code',
+        'is_verified',
+    ];
+
+    //relationship one store to one user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
