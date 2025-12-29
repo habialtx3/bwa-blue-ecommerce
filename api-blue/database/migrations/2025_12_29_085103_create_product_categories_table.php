688e363d1ca13ef9e3d7751314754e92206d6d4a
MREZA <rezaworku31@gmail.com>
2025-12-29 16:04:03 +0700

    created : product category migration and product category recursive created

---
+++ api-blue/database/migrations/2025_12_29_085103_create_product_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('parent_id')->nullable();
+            $table->foreign('parent_id')->references('id')->on('product_categories')->onDelete('cascade');
+            $table->string('image')->nullable();
+            $table->string('name'); //Elektronik rumah
+            $table->string('slug')->unique(); //elektronik-rumah
+            $table->string('tagline');
+            $table->longText( 'description');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
